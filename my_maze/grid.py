6aeea52f0918cbf5d4ac2504d48ee98f75bdaabd
taylorknoles <knolestaylor@gmail.com>
2026-07-28 21:01:01 -0600

    bug fixing

--- my_maze/grid.py
+++ my_maze/grid.py
@@ -1,32 +1,44 @@
 from .cell import Cell
 
 class Grid:
+    """
+    Where the data lives, creates all the cells in a 2d list
+
+    attributes:
+    cells: 2d list, init the cells that the maze class will use
+    height: integer, height of maze
+    width: integer, width of maze
+
+    """
     def __init__(self, width, height, cell_size, color, screen):
         self.cells = [[Cell(row, column, cell_size, color, screen)
                        for column in range(width)]
                       for row in range(height)]
         self.height = height
         self.width = width
+        self.cell_size = cell_size
 
     def __iter__(self):
         return iter(self.cells)
 
     def get_cell(self, row, column):
-
         return self.cells[row][column]
+
+    def get_cellsize(self):
+        return self.cell_size
 
     def neighbors(self, cell):
         row = cell.row
         column = cell.column
         result = []
 
-        if row > 0:
+        if  cell.walls["N"] and row > 0:
             result.append(("N", (self.cells[row-1][column])))
-        if row < self.height - 1:
+        if  cell.walls["S"] and row < self.height - 1:
             result.append(("S", (self.cells[row+1][column])))
-        if column > 0:
+        if  cell.walls["W"] and column > 0:
             result.append(("W", (self.cells[row][column-1])))
-        if column < self.width - 1:
+        if  cell.walls["E"] and column < self.width - 1:
             result.append(("E", (self.cells[row][column+1])))
 
         return result
--- my_maze/maze.py
+++ my_maze/maze.py
@@ -4,42 +4,58 @@ import random
 
 
 class Maze:
+    """
+    Generates a maze using a randomized dfs algorithm
+
+    attributes:
+    grid: Grid class, where the data is stored
+    width: int, the width of the maze
+    height: int, the height of the maze
+    screen: pygame.Surface, the screen where the maze is stored
+    color: tuple, background color of the maze
+    """
+
     def __init__(self, width, height, cell_size, color, screen):
         self.grid = Grid(width, height, cell_size, color, screen)
         self.width = width
         self.height = height
         self.screen = screen
         self.color = color
+        self.cell_size = cell_size
 
     def generate(self, start_row, start_column):
+        # picks starting cell and starts dfs
         start = self.grid.get_cell(start_row, start_column)
         self.dfs(start)
 
-    def remove_walls(self, cell, neighbor, direction):
+    @staticmethod
+    def remove_wall(cell, neighbor, direction):
         opposite = { "N" : "S", "S" : "N", "E" : "W", "W" : "E" }
         cell.walls[direction] = False
         neighbor.walls[opposite[direction]] = False
 
-
     def dfs(self, start):
         self.reset()
         call_stack = [start]
-
+        start.visited = True
 
         while call_stack:
-            cell = call_stack.pop()
-            if not cell.visited:
-                cell.visited = True
+            current = call_stack[-1]
 
-            neighbors = self.grid.neighbors(cell)
-            random.shuffle(neighbors)
+            neighbors = self.grid.neighbors(current)
+            unvisited = [(d, n) for d, n in neighbors if not n.visited]
 
-            for direction, neighbor in neighbors:
-                if not neighbor.visited:
-                    self.remove_walls(cell, neighbor, direction)
-                    call_stack.append(cell)
-                    call_stack.append(neighbor)
-                    break
+            if unvisited:
+                direction, neighbor = random.choice(unvisited)
+
+                self.remove_wall(current, neighbor, direction)
+
+                neighbor.visited = True
+                call_stack.append(neighbor)
+
+            else:
+                call_stack.pop()
+
 
     def reset(self):
         for row in self.grid.cells:
@@ -49,4 +65,5 @@ class Maze:
 
 
     def print(self):
+        # ascii print
         print(self.grid.to_string())
